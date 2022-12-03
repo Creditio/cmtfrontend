a8121b062be88181c780df0d77dd411df70922ba
Soham Sen <hello@sohamsen.me>
2022-12-03 22:58:23 +0530

    Added push notification

--- components/button.tsx
+++ components/button.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-type Props = { text: string, };
+type Props = { text: string, onClick?: React.MouseEventHandler<HTMLButtonElement>, };
 
-const Button = ({ text }: Props) => {
+const Button = ({ text, onClick }: Props) => {
   return (
-    <button type="button" className="focus:outline-none text-white bg-gradient-to-r from-purple-500 to-pink-500  hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 my-6 w-48 h-16">{text}</button>
+    <button
+      type="button"
+      onClick={onClick}
+      className="focus:outline-none text-white bg-gradient-to-r from-purple-500 to-pink-500  hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 my-6 w-48 h-16">{text}</button>
   );
 };
 
--- pages/dashboard/notification.tsx
+++ pages/dashboard/notification.tsx
@@ -1,26 +1,106 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from "../../components/DashboardLayout";
-import Button from "../../components/button";
+import Button from "../../components/Button";
+import * as PushAPI from "@pushprotocol/restapi";
+import { useAccount, useSigner } from 'wagmi';
 
 type Props = {}
 
-export default function notification({}: Props) {
+export default function notification({ }: Props) {
+  const [subject, setSubject] = useState('');
+  const [media, setMedia] = useState('');
+  const [message, setMessage] = useState('');
+  const [link, setLink] = useState('');
+  const [error, setError] = useState('');
+
+  const { address, isConnected } = useAccount();
+  useEffect(() => {
+    if (!isConnected) {
+      setError('Please connect your wallet');
+    } else {
+      setError('');
+    }
+  }, [address, isConnected]);
+
+  const { data: signer } = useSigner({
+    onError(error) {
+      alert(`Error in getting signer: ${error}`);
+      console.log('Error', error);
+    },
+  });
+
+  useEffect(() => {
+    if (!address) return;
+    (async () => {
+      const channelData = await PushAPI.channels.getChannel({
+        channel: `eip155:80001:${address}`,
+        env: 'staging',
+      });
+      if (!channelData) {
+        setError('Please create a channel first');
+      }
+    })();
+  }, []);
+
+  const sendNotification = async () => {
+    if (message == '') {
+      setError('Please enter a message');
+      return;
+    } else {
+      setError('');
+    }
+
+    if (error != '') {
+      alert("Please fix the errors before sending");
+      return;
+    }
+
+    try {
+      const apiResponse = await PushAPI.payloads.sendNotification({
+        signer,
+        type: 1, // to all
+        identityType: 2, // direct payload
+        notification: {
+          title: subject,
+          body: message,
+        },
+        payload: {
+          title: subject,
+          body: message,
+          cta: link,
+          img: media,
+        },
+        channel: `eip155:80001:${address}`,
+        env: 'staging'
+      });
+
+      // apiResponse?.status === 204, if sent successfully!
+      console.log('API repsonse: ', apiResponse);
+      if (apiResponse?.status !== 204) alert("error in sending");
+      else alert("Notification sent!");
+    } catch (err) {
+      console.error('Error: ', err);
+    }
+
+  };
+
   return (
     <Layout>
+      <>
+        {error && <div className="text-red-500">{error}</div>}
         <form className='flex flex-col'>
-        <span className='font-semibold my-2'>Subject</span>
-        <input type="text" className='border-rose-200'/>
-        <span className='font-semibold my-2'>Media</span>
-        <input type="text" className='border-rose-200'/>
+          <span className='font-semibold my-2'>Subject</span>
+          <input type="text" value={subject} className='border-rose-200' onChange={(e) => setSubject(e.target.value)} />
+          <span className='font-semibold my-2'>Media</span>
+          <input type="text" value={media} className='border-rose-200' onChange={(e) => setMedia(e.target.value)} />
+          <span className='font-semibold my-2'>CTA Link</span>
+          <input type="text" value={link} className='border-rose-200' onChange={(e) => setLink(e.target.value)} />
+          <span className='font-semibold my-2'>Notification Message</span>
+          <input type="text" value={message} className='border-rose-200' onChange={(e) => setMessage(e.target.value)} />
 
-        <span className='font-semibold my-2'>CTA Link</span>
-        <input type="text" className='border-rose-200'/>
-
-        <span className='font-semibold my-2'>Notification Message</span>
-        <input type="text" className='border-rose-200'/>
-
-        <Button text="Send Notification"/>
+          <Button text="Send Notification" onClick={sendNotification} />
         </form>
+      </>
     </Layout>
   )
 }
