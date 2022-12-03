6b9a1f3e1ef2b7b2daf10d4d6c5601a5f830d0e6
DaveJay3002 <davejay3002@gmail.com>
2022-12-03 16:55:59 +0530

    Added Card

---
+++ components/card.tsx
@@ -0,0 +1,25 @@
+import react from 'react';
+
+type Props={};
+
+const Card=()=>{
+    return (
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md">
+    <a href="#">
+        <img className="rounded-t-lg" src="/campaign.png" alt="" />
+    </a>
+    <div className="p-5">
+        <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight ">Contributed User Rewards</h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gradient-to-r from-purple-500 to-pink-500 rounded-lg">
+            Edit Campaign
+            <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </a>
+    </div>
+</div>
+    );
+};
+
+export default Card;
