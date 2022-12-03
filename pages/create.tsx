ff134f5a1a7bc98e15598f5c3061a195a668dc79
DaveJay3002 <davejay3002@gmail.com>
2022-12-03 13:41:54 +0530

    Launch Campaign

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Button from "../components/button"
 
 type Props = {};
 
@@ -41,7 +42,7 @@ export default function create({}: Props) {
                 className="border-rose-200 border-2 rounded-lg"
               />
             </div>
-            <div className="my-6 flex flex-col">
+            <div className="my-6 flex flex-col ">
               <span className="font-semibold">Requirements for Claim:- </span>
               <div className="flex items-center my-4 justify-between">
                 <div>
@@ -97,18 +98,47 @@ export default function create({}: Props) {
                     htmlFor="default-checkbox"
                     className="ml-2 text-md font-medium text-gray-900 "
                   >
+                    WorldCoin KYC
+                  </label>
+                </div>
+                <div>
+                  <input
+                    id="default-checkbox"
+                    type="checkbox"
+                    value=""
+                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label
+                    htmlFor="default-checkbox"
+                    className="ml-2 text-md font-medium text-gray-900 "
+                  >
                     Discord Activity
+                  </label>
+                </div>
+                <div>
+                  <input
+                    id="default-checkbox"
+                    type="checkbox"
+                    value=""
+                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label
+                    htmlFor="default-checkbox"
+                    className="ml-2 text-md font-medium text-gray-900 "
+                  >
+                    Enable Push Protocol
                   </label>
                 </div>
               </div>
               <div className="my-6 flex flex-col">
               <span className="font-semibold mb-4">Filter users:- </span>
-              <select className="border-rose-300 border-2 rounded-lg items">
+              <select className="border-rose-300 border-2 rounded-lg items h-12">
                 <option selected>Active users within 30 days.</option>
                 <option>Active users within 60 days.</option>
                 <option>Active users within 120 days.</option>
               </select>
               </div>
+              <Button text="Launch Campaign"/>
             </div>
           </div>
         </form>
