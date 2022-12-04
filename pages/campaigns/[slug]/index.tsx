045bf1ee5947a319eee553a46dbf06e847297609
DaveJay3002 <davejay3002@gmail.com>
2022-12-04 06:59:36 +0530

    xyz

--- pages/campaigns/[slug]/index.tsx
+++ pages/campaigns/[slug]/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Navbar from "../../../components/Navbar";
 import Button from "../../../components/Button";
 import Link from "next/link";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 type Props = {};
 
-export default function campaign({ }: Props) {
+export default function campaign({}: Props) {
   const router = useRouter();
   const { slug } = router.query;
 
@@ -16,18 +16,20 @@ export default function campaign({ }: Props) {
       <div className="justify-center flex flex-col mt-12 w-1/2">
         <img src="/campaign.png" />
         <span className="mt-4 font-bold text-2xl">
-          Contributed user rewards
+          ETH India referral rewards
         </span>
         <div className="flex">
           <div className="flex flex-col w-1/2 my-4 mx-4">
             <span className="mt-4 font-semibold text-lg my-2">
-              Campaign Details
+              About ETH India
             </span>
             <span>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam
-              laudantium atque, illo animi dolorum dignissimos vero voluptatum
-              illum suscipit ullam officiis quos itaque minus optio veritatis,
-              provident unde quibusdam voluptas?
+              ETHIndia is a hackathon — a movement with the goal of inspiring
+              and fostering a community of builders promoting technological
+              advancements for Ethereum. Its a place where the leading minds in
+              the Ethereum ecosystem are uniting with 1500+ builders from around
+              the globe to build decentralized applications for the future. And
+              we invite you to be one of them.
             </span>
             <span className="mt-4 font-semibold text-lg my-2">
               Task Requirements
@@ -61,8 +63,12 @@ export default function campaign({ }: Props) {
               <span className="mx-4 font-bold text-lg">Eth India</span>
             </div>
             <span className="font-bold text-md my-4">Campaign Period</span>
-            <span className="font-light text-gray-700 my-1">From:Thu,November 17,2022</span>
-            <span className="font-light text-gray-700 my-1">End:Thu,December 17,2022</span>
+            <span className="font-light text-gray-700 my-1">
+              From:Thu,November 17,2022
+            </span>
+            <span className="font-light text-gray-700 my-1">
+              End:Thu,December 17,2022
+            </span>
           </div>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,17 +10,13 @@ export default function index({ }: Props) {
     <div className="h-screen bg-white-900 ">
       <Navbar />
       <div className="flex items-center mx-10 my-20">
-        <div className="flex flex-col items-center">
-          <h1 className="text-6xl text-purple-700">Unlocking possibilities in Web3 using verified claims.</h1>
+        <div className="flex flex-col items-center w-1/2">
+          <h1 className="text-6xl text-purple-700">Decentralized, anonymous, secure and efficient credential  issuance protocol </h1>
           <p className="my-12 text-gray-600">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus,
-            nobis culpa. Consectetur, earum placeat, sunt fuga veniam deleniti
-            magni quaerat quae incidunt libero consequuntur distinctio
-            accusantium possimus quia sed facere.
+            Proof of community with ZK proof
           </p>
           <Link href="/dashboard"><Button text="Launch app 🚀" /></Link>
         </div>
-        <img src="/main.png" className="w-1/2 rounded-xl" />
       </div>
     </div>
   );
